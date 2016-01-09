0e5e95473023548e3eb29fd92b9f4e277fa095de
RwaKulszowa <zajactomasz1@yahoo.com>
2016-01-09 11:52:30 +0100

    Implemented TraitEncoder

--- python/trait.py
+++ python/trait.py
@@ -14,6 +14,12 @@ class Trait(object):
         self.elements = elements
         self.categories = []
 
+    def __str__(self):
+        return json.dumps(self, cls=TraitEncoder, indent=4)
+
+    def __repr__(self):
+        return str(self)
+
 class Category(object):
     """
     Each trait divides objects into categories with
@@ -41,4 +47,24 @@ class Relation(object):
 
 class TraitEncoder(json.JSONEncoder):
     def default(self, obj):
+        if isinstance(obj, Trait):
+            return {
+                'name': obj.name,
+                'elements': len(obj.elements)
+            }
+
+        if isinstance(obj, Category):
+            return {
+                'name': obj.name,
+                'elements': len(obj.elements),
+                'value_range': obj.value_range
+            }
+
+        if isinstance(obj, Relation):
+            return {
+                'origin': obj.origin.name,
+                'relative': obj.relative.name,
+                'p': obj.p
+            }
+
         return json.JSONEncoder.default(self, obj)
